779c12f6939c178ea7f6e57dec4355a5b6a90788
DevWorker55 <omnis.devworks@gmail.com>
2016-10-11 22:55:43 -0400

    Added initial projects for MediatR and Hangfire sample projects.

---
+++ Samples/HangfireSample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangfireSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
