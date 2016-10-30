4dab080abe27ae9c21f8234570d40dceba19d611
DevWorker55 <omnis.devworks@gmail.com>
2016-10-30 12:58:52 -0400

    Another sample project.

---
+++ Projects/SkyLine-Tours/SkyLine.Data/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkyLine.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ Projects/SkyLine-Tours/SkyLine.Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SkyLine.Website.Startup))]
+namespace SkyLine.Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
