13ea49c13ed4c453da7dd8ae410ec71c534f4add
devworker55 <omnis.devworks@gmail.com>
2016-11-16 14:45:51 -0500

    Fixed all build errors and dependency references.

--- Samples/GlimpseSample/GlimpseSample.Data/Class1.cs
+++ Samples/GlimpseSample/GlimpseSample.Data/Class1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GlimpseSample.Db
+namespace GlimpseSample.Data
 {
     public class Class1
     {
--- Samples/GlimpseSample/GlimpseSample.Data/Properties/AssemblyInfo.cs
+++ Samples/GlimpseSample/GlimpseSample.Data/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GlimpseSample.Db")]
+[assembly: AssemblyTitle("GlimpseSample.Data")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GlimpseSample.Db")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("GlimpseSample.Data")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("52d596b6-ef86-4f61-8e62-6fb82fdd408b")]
+[assembly: Guid("431dc9c5-4705-445c-9f75-b4ae43bf8379")]
 
 // Version information for an assembly consists of the following four values:
 //
