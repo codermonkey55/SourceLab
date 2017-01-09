71131a58e98d6bb9cad1b27cc7e6a57a62688ac9
DevWorker55 <omnis.devworks@gmail.com>
2017-01-08 19:37:24 -0500

    Updated custome controller factories examples with code to check for mvc child actions as a result of "Html.RendAction" or "Html.Action".

--- Samples/DIWebAppSample/IoC_Core/CastleWindsor/Plumbing/WindsorControllerFactory.cs
+++ Samples/DIWebAppSample/IoC_Core/CastleWindsor/Plumbing/WindsorControllerFactory.cs
@@ -1,7 +1,7 @@
+using Castle.Windsor;
 using System;
 using System.Web.Mvc;
 using System.Web.Routing;
-using Castle.Windsor;
 
 namespace CodeLabs.Web.WebForms.IoC_Integration.IoC_Core.CastleWindsor.Plumbing
 {
@@ -14,8 +14,22 @@ namespace CodeLabs.Web.WebForms.IoC_Integration.IoC_Core.CastleWindsor.Plumbing
             this._container = container;
         }
 
+        /// <summary>
+        /// <see cref="http://stackoverflow.com/questions/21929557/find-if-request-is-child-action-request-before-controller-context-is-available"/>
+        /// <see cref="http://stackoverflow.com/questions/5425920/asp-net-mvc-controller-created-for-every-request"/>
+        /// Asp.NET Mvc calls to get controller for each request.
+        /// </summary>
+        /// <param name="requestContext"></param>
+        /// <param name="controllerType"></param>
+        /// <returns></returns>
         protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
         {
+            var handler = requestContext.HttpContext.CurrentHandler;
+
+            var isChildAction1 = ((MvcHandler)handler).RequestContext.RouteData.DataTokens.ContainsKey("ParentActionViewContext");
+            //-> Or
+            var isChildAction2 = isChildAction1 || requestContext.HttpContext.PreviousHandler != null && requestContext.HttpContext.PreviousHandler is MvcHandler;
+
             if (controllerType != null && _container.Kernel.HasComponent(controllerType))
                 return (IController)_container.Resolve(controllerType);
 
--- Samples/DIWebAppSample/IoC_Core/Ninject/Factories/NinjectControllerFactory.cs
+++ Samples/DIWebAppSample/IoC_Core/Ninject/Factories/NinjectControllerFactory.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿using Ninject;
+using System;
 using System.Web.Mvc;
 using System.Web.Routing;
-using Ninject;
 
 namespace CodeLabs.Web.Mvc5.IoC_Integration.IoC_Core.Ninject.Factories
 {
@@ -14,9 +14,22 @@ namespace CodeLabs.Web.Mvc5.IoC_Integration.IoC_Core.Ninject.Factories
             _kernel = kernel;
         }
 
-        // asp.net mvc calls to get controller for each request
+        /// <summary>
+        /// <see cref="http://stackoverflow.com/questions/21929557/find-if-request-is-child-action-request-before-controller-context-is-available"/>
+        /// <see cref="http://stackoverflow.com/questions/5425920/asp-net-mvc-controller-created-for-every-request"/>
+        /// Asp.NET Mvc calls to get controller for each request.
+        /// </summary>
+        /// <param name="ctx"></param>
+        /// <param name="ctlrType"></param>
+        /// <returns></returns>
         protected override IController GetControllerInstance(RequestContext ctx, Type ctlrType)
         {
+            var handler = ctx.HttpContext.CurrentHandler;
+
+            var isChildAction1 = ((MvcHandler)handler).RequestContext.RouteData.DataTokens.ContainsKey("ParentActionViewContext");
+            //-> Or
+            var isChildAction2 = isChildAction1 || ctx.HttpContext.PreviousHandler != null && ctx.HttpContext.PreviousHandler is MvcHandler;
+
             if (ctlrType == null)
                 return null;
 
