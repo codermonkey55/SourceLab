02487dab00b2065021d98fa7117b70817f475cb1
devworker55 <omnis.devworks@gmail.com>
2017-01-12 20:22:37 -0500

    Added CamelCasedHttpControllerAttribute.

---
+++ Samples/WindowsAuthenticationSample/WindowsAuthenticationSample/Application/Attributes/CamelCasedHttpControllerAttribute.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web.Http.Controllers;
+
+namespace WindowsAuthenticationSample.Application.Attributes
+{
+    public class CamelCasedHttpControllerAttribute
+    {
+        public void SetCamelCasedFormatter(HttpControllerSettings settings, HttpControllerDescriptor descriptor)
+        {
+            var jsonMediaTypeFormatter = settings.Formatters.OfType<JsonMediaTypeFormatter>().SingleOrDefault();
+
+            settings.Formatters.Remove(jsonMediaTypeFormatter);
+
+            jsonMediaTypeFormatter = new JsonMediaTypeFormatter
+            {
+                SerializerSettings = new JsonSerializerSettings
+                {
+                    ContractResolver = new CamelCasePropertyNamesContractResolver()
+                }
+            };
+
+            settings.Formatters.Add(jsonMediaTypeFormatter);
+        }
+    }
+}
