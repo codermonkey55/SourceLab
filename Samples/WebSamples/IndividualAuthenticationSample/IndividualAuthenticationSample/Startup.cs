481011051007342e1f80133d0e52cb34a5242860
DevWorker55 <omnis.devworks@gmail.com>
2017-01-21 12:54:13 -0500

    Added IndividualAuthentication web sample project.

---
+++ Samples/WebSamples/IndividualAuthenticationSample/IndividualAuthenticationSample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IndividualAuthenticationSample.Startup))]
+namespace IndividualAuthenticationSample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
